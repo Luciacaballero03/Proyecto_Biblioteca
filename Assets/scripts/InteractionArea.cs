fe1964666f8396de23573d86317278715223db0f
Luciacaballero03 <lulicaba03@gmail.com>
2026-06-01 14:22:06 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/scripts/InteractionArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public int objetosRecolectados = 0;
+
+    void OnTriggerEnter (Collider col) {
+    if (col.gameObject.CompareTag ("Coleccionable")){
+        objetosRecolectados++;
+        Debug.Log("Has recolectado " + objetosRecolectados + " discos");
+        Destroy(col.gameObject);
+    }
+    }
+}
