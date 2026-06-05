07716fd4161acf022f5d5261d3c8d0d87551ad4a
Luciacaballero03 <lulicaba03@gmail.com>
2026-06-05 14:33:21 -0300

    me falta el ultimo punto de la clase 3

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static float timer = 60f;
+    public UIManager uiManag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManag = Object.FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0) {
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+     public TextMeshProUGUI txt_Score; 
+     public TextMeshProUGUI txt_Temporizador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTimer();
+    }
+
+    public void UpdateScore (int score) {
+        txt_Score.text = "Score: " + score;
+        }
+
+    public void UpdateTimer () {
+        if (GameManager.timer < 0){
+             GameManager.timer = 0;
+        }
+        txt_Temporizador.text = "Tiempo: " + GameManager.timer;
+    } 
+}
